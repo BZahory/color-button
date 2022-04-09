48d9bbd40b130526f5398909e3145011f68b0f79
Bilal <44588480+BZahory@users.noreply.github.com>
2022-04-09 10:28:14 -0400

    Toggle color and text on button click

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import React, {useState} from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [color, setColor] = useState('red');
   return (
-    <div />
+    <div>
+    <button onClick={()=>setColor(color === 'red' ? 'blue' : 'red')} style={{backgroundColor: color}}>change to {color==='red'?'blue' : 'red'}</button>
+    </div>
   );
 }
 
