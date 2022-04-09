d709adcb489fc393506cdee06beb213d35cd9c1a
Bilal <44588480+BZahory@users.noreply.github.com>
2022-04-09 10:59:57 -0400

    Have checkbox control whether button is enabled

--- src/App.js
+++ src/App.js
@@ -4,9 +4,17 @@ import './App.css';
 
 function App() {
   const [color, setColor] = useState('red');
+  const [checked, setChecked] = useState(false);
   return (
     <div>
-    <button onClick={()=>setColor(color === 'red' ? 'blue' : 'red')} style={{backgroundColor: color}}>change to {color==='red'?'blue' : 'red'}</button>
+    <button
+    onClick={()=>setColor(color === 'red' ? 'blue' : 'red')}
+    style={{backgroundColor: color}}
+    disabled={checked}>
+    change to {color==='red'?'blue' : 'red'}
+    </button>
+    <input type="checkbox" checked={checked} onClick={()=>setChecked(!checked)}>
+    </input>
     </div>
   );
 }
